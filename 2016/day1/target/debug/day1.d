bb07c30583fb4140051488043159f65a5ecbb1ab
luk00casati <109902811+luk00casati@users.noreply.github.com>
2024-02-29 19:44:28 +0100

    Delete target directory

--- 2016/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/luca/documents/c-code/c++/aoc/2016/day1/target/debug/day1: /home/luca/documents/c-code/c++/aoc/2016/day1/src/main.rs
--- 2016/day1/target/debug/deps/day1-e68e6dab21047fe3.d
+++
@@ -1,5 +0,0 @@
-/home/luca/documents/c-code/c++/aoc/2016/day1/target/debug/deps/libday1-e68e6dab21047fe3.rmeta: src/main.rs
-
-/home/luca/documents/c-code/c++/aoc/2016/day1/target/debug/deps/day1-e68e6dab21047fe3.d: src/main.rs
-
-src/main.rs:
