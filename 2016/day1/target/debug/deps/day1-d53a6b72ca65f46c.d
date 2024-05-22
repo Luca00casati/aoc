3df7f9987aa6e1b5e144144b43fadfafbb0f7898
Luca Casati <luk00casati@gmail.com>
2024-05-22 22:35:41 +0200

    day 5 1 done

--- 2016/day1/target/debug/deps/day1-d53a6b72ca65f46c.d
+++
@@ -1,5 +0,0 @@
-/home/luca/documents/c-code/aoc/2016/day1/target/debug/deps/libday1-d53a6b72ca65f46c.rmeta: src/main.rs
-
-/home/luca/documents/c-code/aoc/2016/day1/target/debug/deps/day1-d53a6b72ca65f46c.d: src/main.rs
-
-src/main.rs:
